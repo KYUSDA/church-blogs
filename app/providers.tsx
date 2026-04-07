74f7a0f1469368dddda634652182784a3b993c10
sammy6378 <samuel202mwangi@gmail.com>
2026-04-07 13:20:54 +0300

    add web analytics

--- app/providers.tsx
+++ app/providers.tsx
@@ -4,6 +4,7 @@ import { useState } from "react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { HelmetProvider } from "react-helmet-async";
+import { Analytics } from "@vercel/analytics/next";
 
 export function Providers({ children }: { children: React.ReactNode }) {
   const [queryClient] = useState(() => new QueryClient());
@@ -13,6 +14,7 @@ export function Providers({ children }: { children: React.ReactNode }) {
       <QueryClientProvider client={queryClient}>
         {children}
         <ReactQueryDevtools initialIsOpen={false} />
+        <Analytics />
       </QueryClientProvider>
     </HelmetProvider>
   );
